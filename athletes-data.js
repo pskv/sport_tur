a4e6049dd959eb6dcb66241c5e1a4ccb8725acbb
peshi <peshikovs@gmail.com>
2025-12-03 11:28:09 +0300

    Добавил ссылку на рейтинг с кол-вом стартов

--- athletes-data.js
+++ athletes-data.js
@@ -1,6 +1,6 @@
 
 (function() {
-    const DATA_VERSION = '202512030148';
+    const DATA_VERSION = '202512031121';
     const CACHE_KEY = `athletesData_v${DATA_VERSION}`;
     
     // Пытаемся получить данные из кэша
