9d7fea393dcecbaeeac895d5885ddc38e3683ae0
peshi <peshikovs@gmail.com>
2025-12-03 12:46:25 +0300

    Добавил ссылки на индивидуальные рейтинги

--- athletes-data.js
+++ athletes-data.js
@@ -1,6 +1,6 @@
 
 (function() {
-    const DATA_VERSION = '202512031121';
+    const DATA_VERSION = '202512031239';
     const CACHE_KEY = `athletesData_v${DATA_VERSION}`;
     
     // Пытаемся получить данные из кэша
