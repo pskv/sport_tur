7191ecb402b501de9f63590d2d60c9771ba0e54a
peshi <peshikovs@gmail.com>
2025-12-04 13:06:44 +0300

    Удалил карточку рейтинга у тех, у кого нет ни одного.

--- athletes-data.js
+++ athletes-data.js
@@ -1,6 +1,6 @@
 
 (function() {
-    const DATA_VERSION = '202512041210';
+    const DATA_VERSION = '202512041258';
     const CACHE_KEY = `athletesData_v${DATA_VERSION}`;
     
     // Пытаемся получить данные из кэша
