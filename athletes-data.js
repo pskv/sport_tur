5a6480d968a0ae79d024d7248601acf5b99f912e
peshi <peshikovs@gmail.com>
2025-12-08 02:57:05 +0300

    Добавил переход на нужную вкладку на главной

--- athletes-data.js
+++ athletes-data.js
@@ -1,6 +1,6 @@
 
 (function() {
-    const DATA_VERSION = '202512080214';
+    const DATA_VERSION = '202512080255';
     const CACHE_KEY = `athletesData_v${DATA_VERSION}`;
     
     // Пытаемся получить данные из кэша
