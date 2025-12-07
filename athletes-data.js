fd8c383267717fe9fa7c82b9a6508a4ae778b475
peshi <peshikovs@gmail.com>
2025-12-07 17:17:45 +0300

    Поменял стили в рейтингах, соревах и спортсменах

--- athletes-data.js
+++ athletes-data.js
@@ -1,6 +1,6 @@
 
 (function() {
-    const DATA_VERSION = '202512041258';
+    const DATA_VERSION = '202512071611';
     const CACHE_KEY = `athletesData_v${DATA_VERSION}`;
     
     // Пытаемся получить данные из кэша
